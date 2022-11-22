22300aa8a60eab09bc2b802715c020ece2da02fb
prog1700 <andrecelliers3@gmail.com>
2022-11-22 13:30:27 -0400

    Continued work in tutorial game

--- Python-Platformer-main/platform_tutorial.py
+++ Python-Platformer-main/platform_tutorial.py
@@ -1,5 +1,6 @@
-#/usr/bin/python3
-
+#!/usr/bin/python3
+### https://www.youtube.com/watch?v=B6DrRN5z_uU 
+### left off 22:35
 ### importing required modules ###
 import os
 import random
@@ -10,21 +11,50 @@ from os.path import isfile, join
 pygame.init()
 
 ### Sets caption at top of the window ###
-pygame.display.set_caption("Platformer")
+pygame.display.set_caption("Andre's Platformer")
 
 ### Global Variables ###
-BG_COLOR = (255,255,255)
 WIDTH =  1000
 HEIGHT = 800
 FPS = 60
 PLAYER_VEL = 5
 
-#Setting up Pygame Window
+### Setting up Pygame Window ### 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+### Creating player ### 
+class Player(pygame.sprite.Sprite):
+    COLOR = (255,0,0)
+
+    def __init__(self, x, y, width, height):
+        self.rect = pygame.Rect(x,y,width,height)
+        self.y_vel = 0
+    
+
+### Setting up Background function ### 
+def get_background(name):
+    image =  pygame.image.load(join("assets", "Background", name))
+    _, _, width, height = image.get_rect()
+    tiles = []
+
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height +1):
+            pos = (i * width, j * height)
+            tiles.append(pos)
+
+    return tiles, image
+
+#def draw window
+def draw(window,background, bg_image):
+    for tile in background:
+        window.blit(bg_image, tile)
+    pygame.display.update()
+
 
 #Setting up main function
 def main (window):
     clock = pygame.time.Clock()
+    background, bg_image = get_background("Brown.png")
 
     #Define a while loop that runs as event loop
     run = True
@@ -34,6 +64,10 @@ def main (window):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+                break
+        
+        draw(window, background, bg_image) 
+
 
     pygame.quit()
     quit()
